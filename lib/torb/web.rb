7cdac7dbdab5c1bb862b2643e11a4fba3d396378
Taiki Yoshino <44nocycle@gmail.com>
2019-06-17 22:35:07 +0900

    :recycle: Remove unused code

--- lib/torb/web.rb
+++ lib/torb/web.rb
@@ -74,9 +74,7 @@ module Torb
         begin
           event_ids = db.query('SELECT * FROM events ORDER BY id ASC').select(&where).map { |e| e['id'] }
           events = event_ids.map do |event_id|
-            event = get_event(event_id)
-            event['sheets'].each { |sheet| sheet.delete('detail') }
-            event
+            get_event(event_id)
           end
           db.query('COMMIT')
         rescue
